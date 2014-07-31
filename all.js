7b37a172b40963d348e2516f6cc3deac11e43477
Joe Wong <dzinlife@me.com>
2014-08-01 03:14:21 +0800

    form vaildation complete

--- all.js
+++ all.js
@@ -8,13 +8,6 @@ $(document).ready(function() {
 		}
 	});
 
-	$(".form-wrapper").submit(function(e){
-		e.preventDefault();
-		if (true) {
-			$(this).submit();
-		};	
-	})
-
 	$("input, select").focus(function(){
 		$(this).parent(".js-float-label-wrapper ,.js-float-option-wrapper").removeClass("error");
 	})
@@ -22,74 +15,114 @@ $(document).ready(function() {
 	$("select").change(function(){
 		if ($(this)[0].selectedIndex == 0) {
 			$(".reason-description").hide(0);
-		}else if($(this)[0].selectedIndex == 1){
+		}else if($(this)[0].selectedIndex >= 1 && $(this)[0].selectedIndex <= 4){
 			$(".reason-description").show(0);
 			$(".reason-description").children().hide(0);
 			$(".reason-description-refund").show(0);
-		}else if($(this)[0].selectedIndex == 2){
-			$(".reason-description").show(0);
-			$(".reason-description").children().hide(0);
-			$(".reason-description-return").show(0);
-		}else if($(this)[0].selectedIndex == 3){
-			$(".reason-description").show(0);
-			$(".reason-description").children().hide(0);
-			$(".reason-description-exchange").show(0);
-		}else if($(this)[0].selectedIndex == 4){
-			$(".reason-description").show(0);
-			$(".reason-description").children().hide(0);
-			$(".reason-description-repair").show(0);
 		}else if($(this)[0].selectedIndex == 5){
 			$(".reason-description").hide(0);
 		}
 	})
 
-	$("#form-name").blur(function(){
-		if ($(this).val() == '') {
-			$(this).parent().addClass("error");
+	var validate = {
+		"name": function () {
+			if ($("#form-name").val() == '') {
+				$("#form-name").parent().addClass("error");
+				return false;
+			}else{
+				return true;
+			}
+		},
+		"phone": function () {
+			if (!validatePhone($("#form-phone").val())) {
+				$("#form-phone").parent().addClass("error");
+				return false;
+			}else{
+				return true;
+			}
+
+			function validatePhone(number) { 
+				var regex = /(^(([0\+]\d{2,3}-)?(0\d{2,3})-)(\d{7,8})(-(\d{3,}))?$)|(^0{0,1}1[3|4|5|6|7|8|9][0-9]{9}$)/;
+				return regex.test(number);
+			}
+		},
+		"email": function () {
+			if (!validateEmail($("#form-email").val())) {
+				$("#form-email").parent().addClass("error");
+				return false;
+			}else{
+				return true;
+			}
+
+			function validateEmail(email) { 
+				var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+				return regex.test(email);
+			}
+		},
+		"orderNumber": function () {
+			if (!validateOrderNumber($("#form-order-number").val())) {
+				$("#form-order-number").parent().addClass("error");
+				return false;
+			}else{
+				return true;
+			}
+
+			function validateOrderNumber(number) {
+				if (number.length >= 18 && number.length <= 20) {
+					return true;
+				}else{
+					return false;
+				}
+			}
+		},
+		"reason": function() {
+			if ($("#form-reason")[0].selectedIndex == 0) {
+				$("#form-reason").parent().addClass("error");
+				return false;
+			}else{
+				return true;
+			}
 		}
+	}
+
+	$("#form-name").blur(function(){
+		validate.name();
 	});
 
 	$("#form-phone").blur(function(){
-		if (!validatePhone($(this).val())) {
-			$(this).parent().addClass("error");
-		}
-
-		function validatePhone(number) { 
-			var regex = /(^(([0\+]\d{2,3}-)?(0\d{2,3})-)(\d{7,8})(-(\d{3,}))?$)|(^0{0,1}1[3|4|5|6|7|8|9][0-9]{9}$)/;
-			return regex.test(number);
-		}
+		validate.phone();
 	});
 
 	$("#form-email").blur(function(){
-		if (!validateEmail($(this).val())) {
-			$(this).parent().addClass("error");
-		}
-
-		function validateEmail(email) { 
-			var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-			return regex.test(email);
-		}
+		validate.email();
 	});
 
 	$("#form-order-number").blur(function(){
-		if (!validateOrderNumber($(this).val())) {
-			$(this).parent().addClass("error");
-		}
-
-		function validateOrderNumber(number) {
-			if (number.length >= 18 && number.length <= 20) {
-				return 1;
-			}else{
-				return 0;
-			}
-		}
+		validate.orderNumber();	
 	});
 
 	$("#form-reason").blur(function(){
-		if ($(this)[0].selectedIndex == 0) {
-			$(this).parent().addClass("error");
-		}
+		validate.reason();
 	});
 
+	$(".form-wrapper").submit(function(e){
+		var formValidateResault = true;
+		for (n in validate){
+			var thisValidateResault = validate[n]();
+			formValidateResault = formValidateResault && thisValidateResault;
+		};
+
+		if (formValidateResault) {
+			$.ajax({
+				type: "POST",
+				url: "127.0.0.1:8000",
+				data : $(this).serializeArray(),
+			}).done(function () {
+				alert("done");
+			});
+		}
+		e.preventDefault();
+		return false;
+	})
 
 })
